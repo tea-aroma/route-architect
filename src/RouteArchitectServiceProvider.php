a407bf173c4e67debff3ba8bedcac43deb68b5ca
tea-aroma <kovalyovictor71@gmail.com>
2025-04-14 15:58:55 +0500

    first commit.

---
+++ src/RouteArchitectServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TeaAroma\RouteArchitect;
+
+use Illuminate\Support\ServiceProvider;
+
+
+/**
+ * Service provider for the RouteArchitect package.
+ *
+ * @package TeaAroma\RouteArchitect
+ */
+class RouteArchitectServiceProvider extends ServiceProvider
+{
+	/**
+	 * @return void
+	 */
+	public function boot(): void {}
+}
